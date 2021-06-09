027972b6637251e650e9b141c72217b5bb777357
pengsheng.ma <pengsheng.ma@zhaopin.com.cn>
2021-06-09 15:09:32 +0800

    refactor: 修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -183,15 +183,6 @@ module.exports = {
       }
     }],
     'padded-blocks': [2, 'never'],
-    'quotes': [2, 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
-    }],
-    'semi': [2, 'never'],
-    'semi-spacing': [2, {
-      'before': false,
-      'after': true
-    }],
     'space-before-blocks': [2, 'always'],
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath:"static/",
-  //规定@的目录
+  // 规定@的目录
   css: {
     loaderOptions: {
       css: {},
@@ -9,21 +9,21 @@ module.exports = {
           require('postcss-px2rem')({
             // 以设计稿750为例， 750 / 10 = 75
             remUnit: 75
-          }),
+          })
         ]
       },
       less: {
         // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
         // lessOptions: {
-          modifyVars: {
-            // 直接覆盖变量
-            // 'text-color': '#111',
-            // 'border-color': '#eee',
-            // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
-            // hack: `true; @import "F:/Sublime Text 3/code/jybs/xd/src/assets/picker.less";`,
-          },
+        modifyVars: {
+          // 直接覆盖变量
+          // 'text-color': '#111',
+          // 'border-color': '#eee',
+          // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+          // hack: `true; @import "F:/Sublime Text 3/code/jybs/xd/src/assets/picker.less";`,
+        }
         // },
-      },
+      }
     }
-  },
-};
+  }
+}
